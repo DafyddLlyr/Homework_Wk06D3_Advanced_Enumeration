48285ef4ae6c8eb1d9728350e65b46085dc1ac04
dafyddllyr <dafyddllyr@gmail.com>
2019-06-12 15:56:56 +0100

    Isogram finder complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,13 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  let wordObject = this.word.reduce(function (allLetters, letter) {
+    (letter in allLetters) ? allLetters[letter] += 1 : allLetters[letter] = 1;
+    return allLetters;
+  }, {})
+  return Object.values(wordObject).every(frequency => frequency === 1);
 }
 
 module.exports = IsogramFinder;
