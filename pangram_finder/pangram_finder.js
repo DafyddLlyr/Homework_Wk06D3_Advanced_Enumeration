7e32884ba71ff525f471ee5aaa2a9c61ada4f616
dafyddllyr <dafyddllyr@gmail.com>
2019-06-12 15:26:42 +0100

    Pangram finder complete

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  let phraseArray = this.phrase.toLowerCase().split('')
+  let cleanPhraseArray = phraseArray.filter(letter => letter !== /[^abc]/);
+  return this.alphabet.every(letter => cleanPhraseArray.includes(letter))
 }
 
 module.exports = PangramFinder;
