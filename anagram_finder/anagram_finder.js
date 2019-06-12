bc23a21b619867dd8bd24178a9f2680bd3fc46a1
dafyddllyr <dafyddllyr@gmail.com>
2019-06-12 20:31:41 +0100

    Anagram finder completed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,14 @@
 const AnagramFinder = function (word) {
-
+  this.word = word;
+  this.formattedWord = word.toLowerCase().split('')
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  let correctLength = otherWords.filter(word => word.length === this.word.length)
+  return correctLength.filter(word =>
+    (this.formattedWord.every(letter => word.toLowerCase().includes(letter)))
+    && (word !== this.word)
+    && (word !== "") )
 }
 
 module.exports = AnagramFinder;
